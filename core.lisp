8f65b2f6c244387f92597d51cd12e15810ffe7c7
Baggers <chris.bagley@gmail.com>
2016-02-07 23:02:12 +0100

    closer, need to test what I have

--- core.lisp
+++ core.lisp
@@ -9,35 +9,14 @@
 
 ;;----------------------------------------------------------------------
 
-(defparameter *ids* (make-hash-table))
-
-(let ((id -1))
-  (defun %next-id ()
-    (incf id)))
-
-;;----------------------------------------------------------------------
-
 (deftclass event-source
-  (local-combos (make-array 0 :element-type 'function
-			    :adjustable t :fill-pointer 0)))
+  (combos (make-array 0 :element-type 'function
+		      :adjustable t :fill-pointer 0)))
 
-(deftclass (regular-source (:include event-source)))
-
-(deftclass (combo-source (:include event-source))
-  (body (error "Bug found in skitter: combo-source must always be created with a body")))
+(deftclass (combo (:include event-source))
+  (predicate (error "Bug found in skitter: combos must always be created with predicate logic")))
 
 ;;----------------------------------------------------------------------
-
-(deftype source-array ()
-  '(array (or null regular-source) (*)))
-
-(deftclass source-group
-  (sources (make-array 0 :element-type 'source-array
-		       :adjustable t :fill-pointer 0)
-	   :type (array source-array (*)))
-  (global-combos (make-array 0 :element-type 'combo-source
-			     :adjustable t :fill-pointer 0)
-		 :type (array combo-source (*))))
 
 (defmacro defkind (name &body slots)
   (labels ((parse-slot (s)
@@ -51,14 +30,36 @@
 			      (second s)))
 		    (init (if array?
 			      (if (numberp len)
-				  `(make-array ,len :element-type ',elem-type)
+				  `(make-array ,len :element-type ',elem-type
+					       :fill-pointer 0)
 				  `(make-array 0 :element-type ',elem-type
 					       :adjustable t :fill-pointer 0))
 			      `(,(intern (format nil "MAKE-~a" type)
 					 (symbol-package type))))))
 	       `(,name ,init :type ,type))))
-    `(deftclass ,name
-       ,@(mapcar #'parse-slot slots))))
+    (let* ((slot-names (mapcar #'first slots))
+	   (types (mapcar #'second slots))
+	   (accessor-names (mapcar (lambda (x)
+				     (intern (format nil "~a-~a" name x)
+					     (symbol-package name)))
+				   slot-names))
+	   (lengths (mapcar #'third slots)))
+      (when (> (length types) (length (remove-duplicates types)))
+	(error "multiple slots found with the same type, this is no allowed in defkind"))
+      `(progn
+	 (deftclass ,name
+	   ,@(mapcar #'parse-slot slots))
+	 ,@(remove nil
+		   (mapcar (lambda (x a l)
+			     (when l
+			       (let ((push (if (numberp l)
+					       'vector-push
+					       'vector-push-extend)))
+				 `(defmethod add ((inst ,name) (source ,x))
+				    (,push source (,a inst))))))
+			   types
+			   accessor-names
+			   lengths))))))
 
 (defkind mouse
   (pos xy-pos)
@@ -68,34 +69,28 @@
 ;;----------------------------------------------------------------------
 
 (defmacro def-event-source (name &body slots)
-  (let* ((id (or (gethash name *ids*) (%next-id)))
-	 (make (intern (format nil "MAKE-~a" name) (symbol-package name)))
-	 (apply (intern (format nil "APPLY-~a" name) (symbol-package name)))
-	 (add (intern (format nil "ADD-~a" name) (symbol-package name)))
-	 (add-n (intern (format nil "ADD-N-~a" name) (symbol-package name)))
+  (let* ((apply (intern (format nil "APPLY-~a" name) (symbol-package name)))
 	 (slot-names (mapcar #'first slots))
 	 (accessor-names (mapcar (lambda (x)
 				   (intern (format nil "~a-~a" name x)
 					   (symbol-package name)))
 				 slot-names))
-	 (did-set (mapcar #'gensym (mapcar #'symbol-name slot-names))))
+	 (did-set (mapcar #'gensym (mapcar #'symbol-name slot-names)))
+	 (apply-args `(source timestamp
+			      &key ,@(mapcar (lambda (x y) `(,x nil ,y))
+					     slot-names did-set))))
     `(progn
-       (setf (gethash ',name *ids*) ,id)
-       (deftclass (,name (:include regular-source)) ,@slots)
-       (defun ,apply (group index &key ,@(mapcar (lambda (x y) `(,x nil ,y))
-						 slot-names did-set))
-	 (let ((it (aref (aref (source-group-sources group) ,id) index)))
-	   ,@(mapcar (lambda (x y z) `(when ,y (setf (,z it) ,x)))
-		     slot-names did-set accessor-names))
-	 ;; new events :)
-	 )
-       (defun ,add (group)
-	 (let ((g (source-group-sources group)))
-	   (make-n-long g ,id)
-	   (vector-push-extend (,make) (aref g ,id) )))
-       (defun ,add-n (n group)
-	 (loop :for i :below n :do (,add group))
-	 group))))
+       (deftclass (,name (:include event-source)) ,@slots)
+       (defun ,apply ,apply-args
+	 ,@(mapcar (lambda (x y z) `(when ,y (setf (,z source) ,x)))
+		   slot-names did-set accessor-names)
+	 (propagate source timestamp))
+       (define-compiler-macro ,apply ,apply-args
+	 (let ((src (gensym "source")))
+	   `(let ((,src ,source))
+	      ,,@(mapcar (lambda (x y z) `(when ,y (list 'setf (list ',z src) ,x)))
+			 slot-names did-set accessor-names)
+	      ,(list 'propagate src timestamp)))))))
 
 (def-event-source button
   (down-p nil :type boolean))
@@ -106,18 +101,27 @@
 (def-event-source wheel
   (val 0f0 :type single-float))
 
+;; (apply-button x ts :down-p t)
+
 ;;----------------------------------------------------------------------
 
-(defmacro def-combo-event-source (name slots (&key event-var) &body body)
+(defun propagate (source timestamp)
+  (labels ((fire-combo (combo source timestamp)
+	     (funcall (combo-predicate combo) combo source timestamp)))
+    (loop :for c :in (event-source-combos source) :do
+       (fire-combo c source timestamp))))
+
+(defmacro def-combo (name source-types (&key event-var (timestamp-var 'timestamp))
+
+					  slots &body body)
   (assert (symbolp event-var))
+  (assert (listp source-types))
   (let* (;; funcs
 	 (make (intern (format nil "MAKE-~a" name) (symbol-package name)))
+	 (constructor (intern (format nil "%MAKE-~a" name) (symbol-package name)))
 	 (logic (intern (format nil "%~a-BODY" name) (symbol-package name)))
-	 (add (intern (format nil "ADD-~a" name) (symbol-package name)))
 	 (active (intern (format nil "~s-ACTIVE-P" name)
 			 (symbol-package name)))
-	 (body-func (hide (format nil "~s-BODY" name)
-			  (symbol-package name)))
 	 ;;state-slots
 	 (original-slot-names (mapcar #'first slots))
 	 (accessor-names (mapcar (lambda (x)
@@ -130,46 +134,38 @@
 			       slots))
 	 ;;-internal vars
 	 (this (gensym "this")))
+    (assert (and (every #'symbolp source-types)
+		 (> (length source-types) 0)))
     `(progn
-       (deftclass (,name (:include regular-source) (:conc-name nil))
+       (deftclass (,name (:include combo) (:conc-name nil)
+			 (:constructor ,constructor))
 	 ,@hidden-slots
-	 (,body-func #',logic :type function)
 	 (,active nil :type boolean))
-       (defun ,logic (,this ,event-var)
-	 (let ((result
-		(symbol-macrolet
-		    ,(mapcar (lambda (n hn) `(,n (,hn ,event-var)))
-			     original-slot-names hidden-slot-names)
-		  ,@body))
-	       (old-active (,active ,this))
-	       (new-active (not (null result))))
+       (defun ,make ()
+	 (,constructor :predicate #',logic))
+       (defun ,logic (,this ,event-var ,timestamp-var)
+	 (declare (ignorable ,event-var ,timestamp-var))
+	 (let* ((result
+		 (symbol-macrolet
+		     ,(mapcar (lambda (n hn) `(,n (,hn ,event-var)))
+			      original-slot-names hidden-slot-names)
+		   ,@body))
+		(old-active (,active ,this))
+		(new-active (not (null result))))
 	   (setf (,active ,this) new-active)
 	   (when (not (eq old-active new-active))
-	     ;; new events :)
-	     )
+	     (propagate ,this ,timestamp-var))
 	   nil))
-       (defun ,add (group &rest listening-to)
-	 (let* ((g (source-group-combos group)))
-	   (vector-push-extend (,make) (source-group-combos group))
-	   (loop :for (source-kind source-index) :across listening-to :do
-	      (let ((source-kind-index (or (gethash ))))
-		(vector-push-extend #',logic
-				    (aref (aref (source-group-sources group)
-						source-kind-index)
-					  source-index))))
-	   group)))))
+       (defmethod add ((inst ,name) source)
+	 ,(when source-types
+		`(assert (or ,@(mapcar (lambda (s) `(typep inst ',s))
+				       source-types))))
+	 (vector-push-extend inst (event-source-combos source))
+	 source))))
 
-(def-combo-event-source double-click
+(def-combo double-click (button) (:event-var evt)
     ((last-down 0 :type fix))
-    (:event-var evt)
   1)
 
+
 ;;----------------------------------------------------------------------
-
-(defvar +mice+
-  (make-array 0 :element-type 'source-group
-	      :adjustable t :fill-pointer 0))
-
-(defvar +keyboards+
-  (make-array 0 :element-type 'source-group
-	      :adjustable t :fill-pointer 0))
