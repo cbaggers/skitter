0a922760252d6c36536a7c66cacb8dfbc697dbce
cbaggers <chris.bagley@gmail.com>
2017-06-23 18:29:46 +0100

    naming issue. shit why didnt this show in asdf verbose?

--- common.lisp
+++ common.lisp
@@ -24,7 +24,7 @@
 (define-input-source mouse (:static t)
   (pos vec2-control)
   (move vec2-decaying-control)
-  (float-control vec2-control)
+  (wheel vec2-control)
   (button boolean-control *))
 
 (define-input-source gamepad ()
